5af9ed7dda2fb9c2f5c32ac8a64b53bc0d8f11d1
tien998 <tienvn998@gmai.com>
2025-07-03 11:30:11 +0700

    add product form client validate

--- src/pages/products/comps/productForm.tsx
+++ src/pages/products/comps/productForm.tsx
@@ -1,7 +1,11 @@
-import { Form, useNavigation, type HTMLFormMethod } from "react-router";
+import { useFetcher, useNavigation, type HTMLFormMethod } from "react-router";
 import F, { Input, TextArea } from "../../../components/UI/input";
 import useTwoWayBinding from "../../../hooks/useTwoWayBinding";
 import type { IProduct } from "../../../interfaces/product";
+import useValidate from "../../../hooks/useValidate";
+import { isNotNull } from "../../../ulties/inputValidationUltil/validate";
+import ErrorMsg from "../../../components/UI/ErrorMsg";
+import { useRef, useState, type FormEvent } from "react";
 
 
 type Props = {
@@ -15,6 +19,9 @@ export default function ProdForm({ product, action, method, isLoading = false }:
 
     const { state } = useNavigation()
 
+    const fileInputRef = useRef<HTMLInputElement>(null)
+
+
     const [name, onChangeName] = useTwoWayBinding(product?.name ?? '')
     const [category, onChangeCategory] = useTwoWayBinding(product?.category ?? '')
     const [quantity, onChangeQuantity] = useTwoWayBinding(product?.totalQuantity ?? '0')
@@ -22,38 +29,89 @@ export default function ProdForm({ product, action, method, isLoading = false }:
     const [shortDesc, onChangeShortDesc] = useTwoWayBinding(product?.short_desc ?? '')
     const [longDesc, onChangeLongDesc] = useTwoWayBinding(product?.long_desc ?? '')
 
+    const invalidName = useValidate('Name', name, [isNotNull])
+    const invalidCategory = useValidate('Category', category, [isNotNull])
+    const invalidQuantity = useValidate('Quantity', quantity, [isNotNull])
+    const invalidPrice = useValidate('Price', price, [isNotNull])
+    const invalidShortDesc = useValidate('Short description', shortDesc, [isNotNull])
+    const invalidLongDesc = useValidate('Long description', longDesc, [isNotNull])
+
+    const [isSubmited, setIsSubmited] = useState(false)
+    const submit = useFetcher().submit
+
+    function onSubmit(e: FormEvent) {
+        e.preventDefault()
+        setIsSubmited(true)
+
+        if (invalidName || invalidCategory || invalidQuantity || invalidPrice || invalidShortDesc || invalidLongDesc)
+            return
+
+        const formData = new FormData()
+
+        // Thêm các text fields
+        formData.append('name', name)
+        formData.append('category', category)
+        formData.append('price', String(price))
+        formData.append('totalQuantity', String(quantity))
+        formData.append('short_desc', shortDesc)
+        formData.append('long_desc', longDesc)
+
+        // Thêm files nếu có
+        if (fileInputRef.current?.files) {
+            const files = fileInputRef.current.files
+            for (let i = 0; i < files.length; i++) {
+                formData.append('files', files[i])
+            }
+        }
+
+
+        submit(formData, {
+            action, method, encType: 'multipart/form-data'
+        })
+    }
+
     return (
-        <Form action={action} method={method} encType="multipart/form-data">
+        <form onSubmit={onSubmit}>
             <F.Container label="Product Name">
                 <Input placeholder="Enter Product Name" disabled={isLoading}
                     name='name' value={name} onChange={onChangeName} />
+                <ErrorMsg msg={(isSubmited && invalidName) ? invalidName : ''} />
             </F.Container>
 
             <F.Container label="Price">
                 <Input placeholder="Enter Price" disabled={isLoading}
                     name='price' value={price} onChange={onChangePrice} />
+                <ErrorMsg msg={(isSubmited && invalidPrice) ? invalidPrice : ''} />
+
             </F.Container>
 
             <F.Container label="Category">
                 <Input placeholder="Enter Category" disabled={isLoading}
                     name='category' value={category} onChange={onChangeCategory} />
+                <ErrorMsg msg={(isSubmited && invalidCategory) ? invalidCategory : ''} />
+
             </F.Container>
 
             <F.Container label="Quantity">
                 <Input placeholder="Enter In Stoke Quantity" disabled={isLoading} type="number"
                     name='totalQuantity' value={quantity} onChange={onChangeQuantity} />
+                <ErrorMsg msg={(isSubmited && invalidQuantity) ? invalidQuantity : ''} />
+
             </F.Container>
 
             <F.Container label="Short Description" >
                 <TextArea placeholder="Enter Short Description" disabled={isLoading}
                     rows={3}
                     name='long_desc' value={shortDesc} onChange={onChangeShortDesc} />
+                <ErrorMsg msg={(isSubmited && invalidShortDesc) ? invalidLongDesc : ''} />
             </F.Container>
 
             <F.Container label="Long Description">
                 <TextArea placeholder="Enter Long Description" disabled={isLoading}
                     rows={5}
                     name='short_desc' value={longDesc} onChange={onChangeLongDesc} />
+                <ErrorMsg msg={(isSubmited && invalidLongDesc) ? invalidLongDesc : ''} />
+
             </F.Container>
 
             {/* <F.Container label="Upload image (5 images)">
@@ -63,7 +121,7 @@ export default function ProdForm({ product, action, method, isLoading = false }:
 
             <div className="mb-4">
                 <label className="block text-sm font-semibold text-gray-700">Upload image (5 images)</label>
-                <input disabled={isLoading}
+                <input disabled={isLoading} ref={fileInputRef}
                     placeholder="image"
                     type="file"
                     name="files"
@@ -79,6 +137,6 @@ export default function ProdForm({ product, action, method, isLoading = false }:
             >
                 Submit
             </button>
-        </Form>
+        </form>
     )
 }
