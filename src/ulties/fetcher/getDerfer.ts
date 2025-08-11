da9d360768b5e5635187afdd3cddd90c3ccd6bf0
tien998 <tienvn998@gmail.com>
2025-08-11 15:48:05 +0700

    add secure header for CORs

--- src/ulties/fetcher/getDerfer.ts
+++ src/ulties/fetcher/getDerfer.ts
@@ -1,7 +1,10 @@
 export async function getDefer<T = any>(
     url: string, includeCookie: boolean = false, token?: string
 ): Promise<T | null> {
-    const headers: HeadersInit = {}
+    const headers: HeadersInit = {
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Origin': 'https://minhtien-admin.vercel.app'
+    }
     if (token)
         headers['Authorization'] = `Bearer ${token}`
 
